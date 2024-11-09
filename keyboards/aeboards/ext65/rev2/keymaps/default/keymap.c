877a8165848ac55a60c339958f7af22ac4235932
LewisByrne11 <38484293+LewisByrne11@users.noreply.github.com>
2024-11-09 20:07:28 +1100

    tweaks

--- keyboards/aeboards/ext65/rev2/keymaps/default/keymap.c
+++ keyboards/aeboards/ext65/rev2/keymaps/default/keymap.c
@@ -80,12 +80,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef OLED_ENABLE
 // WPM-responsive animation stuff here
 #    define IDLE_FRAMES 5
-#    define IDLE_SPEED 20  // below this wpm value your animation will idle
+#    define IDLE_SPEED 0  // below this wpm value your animation will idle
 
-// #define PREP_FRAMES 1 // uncomment if >1
+ #define PREP_FRAMES 1 // uncomment if >1
 
 #    define TAP_FRAMES 2
-#    define TAP_SPEED 40  // above this wpm value typing animation to trigger
+#    define TAP_SPEED 0  // above this wpm value typing animation to trigger
 
 #    define ANIM_FRAME_DURATION 200  // how long each frame lasts in ms
 // #define SLEEP_TIMER 60000 // should sleep after this period of 0 wpm, needs fixing
