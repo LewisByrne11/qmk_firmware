1530de924be6fe44944cce1a620430eb44e6900c
LewisByrne11 <38484293+LewisByrne11@users.noreply.github.com>
2024-11-09 20:26:56 +1100

    anim?

--- keyboards/aeboards/ext65/rev2/keymaps/default/keymap.c
+++ keyboards/aeboards/ext65/rev2/keymaps/default/keymap.c
@@ -177,7 +177,7 @@ static void render_anim(void) {
 
 // Used to draw on to the oled screen
 bool oled_task_user(void) {
-    render_anim();  // renders pixelart
+    animation_phase();  // renders pixelart
 
     oled_set_cursor(0, 0);                            // sets cursor to (row, column) using charactar spacing (5 rows on 128x32 screen, anything more will overflow back to the top)
     sprintf(wpm_str, "WPM:%03d", get_current_wpm());  // edit the string to change wwhat shows up, edit %03d to change how many digits show up
